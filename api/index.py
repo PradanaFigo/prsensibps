5251493bef04538449ef9b933a0314c91be45253
Pradana Figo Ariasya_123140063 <pradana.123140063@student.itera.ac.id>
2026-08-12 15:05:16 +0700

    Mount FastAPI app under /api for Vercel routing

--- api/index.py
+++ api/index.py
@@ -4,4 +4,8 @@ import os
 # Tambahkan folder backend ke dalam system path agar impor 'app.xxx' bisa terbaca oleh Python di Vercel
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'backend'))
 
-from app.main import app
+from app.main import app as original_app
+from fastapi import FastAPI
+
+app = FastAPI()
+app.mount("/api", original_app)
